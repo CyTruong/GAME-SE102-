f7b68dfaf7b9165b16edf42c69f0effd8b8ada53
cylasion <phamnhattruong.dev@gmail.com>
2018-10-30 09:55:54 +0700

    Nothing special

---
+++ WIP/Game/Map.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "rapidjson\document.h"
+#include <d3d9.h>
+#include <d3dx9.h>
+class Map
+{
+public:
+	char* JsonFileName;
+	HWND hWnd;
+public:
+	Map(HWND,char*);
+	void Creat();
+	~Map();
+};
+
